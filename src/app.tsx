11409d8b5689085c62e2085702a9df277b58e296
kkuang70 <veinkuang@gmail.com>
2020-11-30 17:45:46 -0500

    Initial Pro Ant UI Dashboard

---
+++ src/app.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import { BasicLayoutProps, Settings as LayoutSettings, PageLoading } from '@ant-design/pro-layout';
+import { notification } from 'antd';
+import { history, RequestConfig } from 'umi';
+import RightContent from '@/components/RightContent';
+import Footer from '@/components/Footer';
+import { ResponseError } from 'umi-request';
+import { queryCurrent } from './services/user';
+import defaultSettings from '../config/defaultSettings';
+
+/**
+ * 获取用户信息比较慢的时候会展示一个 loading
+ */
+export const initialStateConfig = {
+  loading: <PageLoading />,
+};
+
+export async function getInitialState(): Promise<{
+  settings?: LayoutSettings;
+  currentUser?: API.CurrentUser;
+  fetchUserInfo?: () => Promise<API.CurrentUser | undefined>;
+}> {
+  const fetchUserInfo = async () => {
+    try {
+      const currentUser = await queryCurrent();
+      return currentUser;
+    } catch (error) {
+      history.push('/user/login');
+    }
+    return undefined;
+  };
+  // If it is a login page, do not execute
+  if (history.location.pathname !== '/user/login') {
+    const currentUser = await fetchUserInfo();
+    return {
+      fetchUserInfo,
+      currentUser,
+      settings: defaultSettings,
+    };
+  }
+  return {
+    fetchUserInfo,
+    settings: defaultSettings,
+  };
+}
+
+export const layout = ({
+  initialState,
+}: {
+  initialState: { settings?: LayoutSettings; currentUser?: API.CurrentUser };
+}): BasicLayoutProps & {
+  childrenRender?: (dom: JSX.Element) => React.ReactNode;
+} => {
+  return {
+    rightContentRender: () => <RightContent />,
+    disableContentMargin: false,
+    footerRender: () => <Footer />,
+    onPageChange: () => {
+      const { currentUser } = initialState;
+      const { location } = history;
+      // If not logged in, redirect to login
+      if (!currentUser && location.pathname !== '/user/login') {
+        history.push('/user/login');
+      }
+    },
+    menuHeaderRender: undefined,
+    ...initialState?.settings,
+  };
+};
+
+const codeMessage = {
+  200: 'The server successfully returned the requested data.',
+  201: 'New or modified data is successful.',
+  202: 'A request has entered the background queue (asynchronous task).',
+  204: 'The data was deleted successfully.',
+  400: 'There was an error in the request sent, and the server did not create or modify data.',
+  401: 'The user does not have permission (the token, username, password is wrong).',
+  403: 'The user is authorized, but access is forbidden.',
+  404: 'The request sent was for a record that did not exist, and the server did not operate.',
+  405: 'The request method is not allowed.',
+  406: 'The requested format is not available.',
+  410: 'The requested resource is permanently deleted and will no longer be available.',
+  422: 'When creating an object, a validation error occurred.',
+  500: 'An error occurred on the server, please check the server.',
+  502: 'Gateway error.',
+  503: 'The service is unavailable, and the server is temporarily overloaded or maintained.',
+  504: 'The gateway timed out.',
+};
+
+/**
+ * 异常处理程序
+ */
+const errorHandler = (error: ResponseError) => {
+  const { response } = error;
+  if (response && response.status) {
+    const errorText = codeMessage[response.status] || response.statusText;
+    const { status, url } = response;
+
+    notification.error({
+      message: `请求错误 ${status}: ${url}`,
+      description: errorText,
+    });
+  }
+
+  if (!response) {
+    notification.error({
+      description: '您的网络发生异常，无法连接服务器',
+      message: '网络异常',
+    });
+  }
+  throw error;
+};
+
+export const request: RequestConfig = {
+  errorHandler,
+};
